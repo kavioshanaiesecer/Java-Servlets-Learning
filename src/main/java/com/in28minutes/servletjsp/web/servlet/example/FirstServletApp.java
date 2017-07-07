62f79d57858ab180daad08e616352461da19b7d2
kavioshanaiesecer <kavi.oshan8@gmail.com>
2017-07-07 06:57:59 +0530

    interface navigation is fixed

---
+++ src/main/java/com/in28minutes/servletjsp/web/servlet/example/FirstServletApp.java
@@ -0,0 +1,10 @@
+package com.in28minutes.servletjsp.web.servlet.example;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+
+@WebServlet(urlPatterns="/first")
+public class FirstServletApp extends HttpServlet{
+
+}
+
